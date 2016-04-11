42e5ed2aa86ef87626be312584719fe9b696f034
Coleman Kelleghan <coleman@kelleghandesign.com>
2016-04-11 11:17:12 -0600

    initial

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('knex')(require('../knexfile')['development']);
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+
+
+router.get('/shirt/:id', function(req, res, next) {
+  var shirtid = req.params.id;
+    res.render('shirt', { shirt: shirt });
 });
+
+
+
+router.get('/', function(req, res, next) {
+    res.render('index', { shirts: shirts });
+});
+
+
 
 module.exports = router;
