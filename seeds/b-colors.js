3d354e2ed1fad18c59c4f54b29170aa4c4559ffd
Wfuger <wfuger@gmail.com>
2016-04-12 10:10:09 -0600

    seeds fixed and inventory updated

--- seeds/b-colors.js
+++ seeds/b-colors.js
@@ -5,10 +5,10 @@ exports.seed = function(knex, Promise) {
     knex('colors').del(),
 
     // Inserts seed entries
-    knex('colors').insert({id: 1, color: 'red'}),
-    knex('colors').insert({id: 2, color: 'white'}),
-    knex('colors').insert({id: 3, color: 'blue'}),
-    knex('colors').insert({id: 4, color: 'black'}),
-    knex('colors').insert({id: 5, color: 'grey'})
+    knex('colors').insert({color: 'red'}),
+    knex('colors').insert({color: 'white'}),
+    knex('colors').insert({color: 'blue'}),
+    knex('colors').insert({color: 'black'}),
+    knex('colors').insert({color: 'grey'})
   );
 };
--- seeds/d-image-url.js
+++ seeds/d-image-url.js
@@ -5,17 +5,16 @@ exports.seed = function(knex, Promise) {
     knex('shirt_image_urls').del(),
 
     // Inserts seed entries
-    knex('shirt_image_urls').insert({id: 1, url: '../images/astroblack.jpg'}),
-    knex('shirt_image_urls').insert({id: 2, url: '../images/cityblue.jpg'}),
-    knex('shirt_image_urls').insert({id: 3, url: '../images/citygrey.jpg'}),
-    knex('shirt_image_urls').insert({id: 4, url: '../images/citywhite.jpg'}),
-    knex('shirt_image_urls').insert({id: 5, url: '../images/flagblack.jpg'}),
-    knex('shirt_image_urls').insert({id: 6, url: '../images/flagblue.jpg'}),
-    knex('shirt_image_urls').insert({id: 7, url: '../images/flagred.jpg'}),
-    knex('shirt_image_urls').insert({id: 8, url: '../images/flagwhite.jpg'}),
-    knex('shirt_image_urls').insert({id: 9, url: '../images/soldierwhite.jpg'}),
-    knex('shirt_image_urls').insert({id: 10, url: '../images/logoblack.jpg'}),
-    knex('shirt_image_urls').insert({id: 11, url: '../images/logowhite.jpg'}),
-    knex('shirt_image_urls').insert({id: 12, url: '../images/astrowhite.jpg'})
+    knex('shirt_image_urls').insert({url: '../images/astroblack.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/cityblue.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/citygrey.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/citywhite.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/flagblack.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/flagblue.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/flagred.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/flagwhite.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/soldierwhite.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/logoblack.jpg'}),
+    knex('shirt_image_urls').insert({url: '../images/logowhite.jpg'})
   );
 };
