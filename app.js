f99238c6a0ee1d4c2996b8b0c4f564b0a8e956d3
Wfuger <wfuger@gmail.com>
2016-04-14 12:26:39 -0600

    plus minus works like a dream

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var hbs = require('hbs');
 hbs.registerHelper('equal', require('handlebars-helper-equal'));
+hbs.registerHelper('compare', function(lvalue, rvalue, options) {
+		if (arguments.length < 3)
+			throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+		var operator = options.hash.operator || "==";
+		var operators = {
+			'==':		function(l,r) { return l == r; },
+			'===':	function(l,r) { return l === r; },
+			'!=':		function(l,r) { return l != r; },
+			'<':		function(l,r) { return l < r; },
+			'>':		function(l,r) { return l > r; },
+			'<=':		function(l,r) { return l <= r; },
+			'>=':		function(l,r) { return l >= r; },
+			'typeof':	function(l,r) { return typeof l == r; }
+		}
+		if (!operators[operator])
+			throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
+		var result = operators[operator](lvalue,rvalue);
+		if( result ) {
+			return options.fn(this);
+		} else {
+			return options.inverse(this);
+		}
+	});
 var bookshelf = require('./db/config.js')
 
 hbs.registerPartial('cart', fs.readFileSync(__dirname + '/views/partials/_cart.hbs', 'utf8'));
